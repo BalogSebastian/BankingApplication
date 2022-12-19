0e5bc44ba57e1aa5eafd57e048b85fcbb419c8af
balogmate <balogmate@4sys.hu>
2022-12-20 00:46:16 +0100

    Making changes in order to start right the project

--- untitled/src/ScannerTest.java
+++ untitled/src/ScannerTest.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class ScannerTest {
 
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a charachter");
-        //
+        int a = scanner.nextInt();
         char c = scanner.next().charAt(0); // get me the first charachter
         System.out.println("value = " + c);
     }
